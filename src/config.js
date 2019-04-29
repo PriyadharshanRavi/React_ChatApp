0e6c5719ccfb127e4c3f0dc8888806293a09dba2
PriyadharshanRavi <mailmefriends95@gmail.com>
2019-04-29 17:32:00 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/de5c643a-95f1-45eb-9c84-0040dfdbe904/token";
+const instanceLocator = "v1:us1:de5c643a-95f1-45eb-9c84-0040dfdbe904";
+
+export { tokenUrl, instanceLocator }
