2d0153ff68926646d28c5991c5d3d86b9d27005c
PriyadharshanRavi <mailmefriends95@gmail.com>
2019-04-30 13:36:37 +0530

    push build

---
+++ build/precache-manifest.c0b9c154d6c145837b265abd938911b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f734ff17a09c074a9a163efeeebc51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb91c67d97673bd9e16d",
+    "url": "/static/css/main.9164eeec.chunk.css"
+  },
+  {
+    "revision": "452cad55e94d6eaa17af",
+    "url": "/static/js/2.84cc8e51.chunk.js"
+  },
+  {
+    "revision": "bb91c67d97673bd9e16d",
+    "url": "/static/js/main.168cfa04.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
